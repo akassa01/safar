0654df73426d6aa60755b2c87e6eafa8a934c57c
akassa01 <165813708+akassa01@users.noreply.github.com>
2026-02-19 23:54:22 +0100

    fix<ci>: changed ci script path

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 # Generate Secrets.xcconfig from Xcode Cloud environment variables
-cat > "$CI_WORKSPACE/Secrets.xcconfig" << EOF
-UNSPLASH_ACCESS_KEY = $UNSPLASH_ACCESS_KEY
-EOF
+echo "UNSPLASH_ACCESS_KEY = $UNSPLASH_ACCESS_KEY" > "$CI_PRIMARY_REPOSITORY_PATH/Secrets.xcconfig"
